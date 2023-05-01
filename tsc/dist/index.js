af658a896a504858704ff7c2c48cb230e4ed5973
Tonmoy kumar Roy <tonmoykumarroy6@gmail.com>
2023-05-02 01:17:23 +0600

    feat(tsc_basic)
    tsc configfile and environment setup

---
+++ tsc/dist/index.js
@@ -0,0 +1,3 @@
+"use strict";
+const course = "my name is tonmoy kumar roy chamak";
+console.log(course);
---
+++ tsc/src/index.ts
@@ -0,0 +1,5 @@
+const course: string = "my name is tonmoy kumar roy chamak";
+const money: number = 120;
+
+console.log(course);
+console.log(money);
